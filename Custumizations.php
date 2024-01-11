75a42d66955a87bf54889dca60ec8aae9682efec
GuiM3squita <guimesquita.1512@gmail.com>
2024-01-11 14:18:46 -0300

    alteraçoes do css da segunda pagina
    a maioria no texto

--- Custumizations.php
+++ Custumizations.php
@@ -7,30 +7,40 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha384-xmcXMp9XbBdTEu4jAxLEL1TV9w8jxL3+mq4NtDQ9w5KI7jMjMJKz5RlPmk8b0CRf" crossorigin="anonymous">
     <link rel="stylesheet" href="css/estilo.css">
 
+
     <title>Street Elegance</title>
 </head>
+
 <body>
 
 
-        <!--NavBar-->
-    <?php
-      require_once './components/header-user.php'
 
-    ?>
-
-    <div id="dicas">
+    <div class="dicas">
     <h1 >Dicas para seu estilo</h1>
 
-    <p>1-Identifique o estilo que mais combina com sua personalidade. Lembre-se de que você pode transitar entre diferentes estilos, e os exemplos na primeira página são apenas referências. <br>
-       2-Escolha um estilo que faça você se sentir confortável e autêntico. Analise as peças que já possui em seu guarda-roupa e faça uma lista do que precisa adquirir para aprimorar seu visual, utilizando nosso site como guia. <br>
-       3-Priorize roupas em cores neutras, pois são fáceis de combinar. A maioria dos estilos valoriza peças mais simples, como camisetas lisas e calças no estilo escolhido. Invista em peças curingas para garantir um gasto consciente.<br>
-       4-Dê atenção especial aos sneakers, escolhendo preferencialmente um branco e um preto. Essas opções são versáteis e se encaixam em diversos looks, agregando estilo ao seu visual.<br>
+    <p>1-Identifique o estilo que mais combina com sua personalidade: Lembre-se de que você pode transitar entre diferentes estilos, e os exemplos na primeira página são apenas referências. <br><br>
+       2-Escolha um estilo que faça você se sentir confortável e autêntico: Analise as peças que já possui em seu guarda-roupa e faça uma lista do que precisa adquirir para aprimorar seu visual, utilizando nosso site como guia. <br><br>
+       3-Priorize roupas em cores neutras, pois são fáceis de combinar: A maioria dos estilos valoriza peças mais simples, como camisetas lisas e calças no estilo escolhido. Invista em peças curingas para garantir um gasto consciente.<br><br>
+       4-Dê atenção especial aos tênis: escolhendo preferencialmente um branco e um preto. Essas opções são versáteis e se encaixam em diversos looks, agregando estilo ao seu visual.<br><br>
        5-Após renovar seu guarda-roupa, invista em acessórios e peças características do estilo escolhido. Isso proporcionará um toque final, garantindo que seu guarda-roupa seja totalmente versátil e reflita sua personalidade.
     </p>
+    <br><br><br>
 
-    
-
+    <h2>Clique em saiba mais no estilo que deseja</h2>
+    <p>Você receberá informações para compreender mais afundo o estilo que combine com você</p>
     </div>
     
+    <div class="card" style="width: 18rem;">
+  <img src="/img/st3_card.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Streetwear</h5>
+    <p class="card-text"></p>
+    <a href="#" class="btn btn-primary">Saiba mais</a>
+  </div>
+</div>
+
+    
+
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </html>
