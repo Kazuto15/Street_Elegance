619130412495f8753b1c3f2bcc1cf7c8e95c14e8
GuiM3squita <guimesquita.1512@gmail.com>
2024-01-10 21:22:52 -0300

    adicionei o css da segunda pagina
    e isso

--- Custumizations.php
+++ Custumizations.php
@@ -3,7 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha384-xmcXMp9XbBdTEu4jAxLEL1TV9w8jxL3+mq4NtDQ9w5KI7jMjMJKz5RlPmk8b0CRf" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/estilo.css">
+
     <title>Street Elegance</title>
 </head>
 <body>
@@ -14,16 +17,18 @@
       require_once './components/header-user.php'
 
     ?>
-    
-    <div class="dicas">
+
+    <div id="dicas">
     <h1 >Dicas para seu estilo</h1>
 
-    <p>1-Veja qual estilo se adéqua a você e sua perssonalidade. Lembrando você pode variar de um estilo para outro os estilos da primeira pagina são apenas exemplos. <br>
-       2-Escolha um estilo que você se sente bem e representado.Veja suas peças no guarda-roupa e crie uma lista do que necessita para que você se vista melhor aqui no site mesmo. <br>
-       3-Opte primeiro por roupas com cores neutras, facil de combinar e quase todos os estilos são utilizadas roupas mais simples, como camistas lisas, calças do estilo escolhido e compre peças coringas para não ser um dinheiro mal gasto. <br>
-       4-Agpra, uma parte excencial para seu estilo, os Snekears escolha de preferencia um branco e um preto sendo muito versartil em diversos looks.
-
+    <p>1-Identifique o estilo que mais combina com sua personalidade. Lembre-se de que você pode transitar entre diferentes estilos, e os exemplos na primeira página são apenas referências. <br>
+       2-Escolha um estilo que faça você se sentir confortável e autêntico. Analise as peças que já possui em seu guarda-roupa e faça uma lista do que precisa adquirir para aprimorar seu visual, utilizando nosso site como guia. <br>
+       3-Priorize roupas em cores neutras, pois são fáceis de combinar. A maioria dos estilos valoriza peças mais simples, como camisetas lisas e calças no estilo escolhido. Invista em peças curingas para garantir um gasto consciente.<br>
+       4-Dê atenção especial aos sneakers, escolhendo preferencialmente um branco e um preto. Essas opções são versáteis e se encaixam em diversos looks, agregando estilo ao seu visual.<br>
+       5-Após renovar seu guarda-roupa, invista em acessórios e peças características do estilo escolhido. Isso proporcionará um toque final, garantindo que seu guarda-roupa seja totalmente versátil e reflita sua personalidade.
     </p>
+
+    
 
     </div>
     
